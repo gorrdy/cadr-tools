f66e725987dfcea44618171a85cec82d89339f2f
Jan Dvorak <dvorakh1997@gmail.com>
2023-11-21 15:22:23 +0100

    add hack for the bullseye release until there is a tor-hs-patch-config in bullseye repo

--- set_repositories.sh
+++ set_repositories.sh
@@ -23,4 +23,9 @@ gpg --export BC528686B50D79E339D3721CEB3E94ADBE1229CF | apt-key add -
 echo "deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/debian/$id/prod $codename main" | tee /etc/apt/sources.list.d/microsoft.list > /dev/null
 echo "deb [signed-by=3D9E81D3CA76CDCBE768C4B4DC6B4F8E60B8CF4C] https://deb.ln-ask.me/beta $codename common local desktop" | tee /etc/apt/sources.list.d/cryptoanarchy.list > /dev/null
 
+#hack the new installation due to missing package of tor-hs-patch-config in "deb.ln-ask.me/beta bullseye" until it is resolved. Get it from the "deb.ln-ask.me/beta buster".
+if [[ "$codename" = "bullseye" ]]; then
+  echo "deb [signed-by=3D9E81D3CA76CDCBE768C4B4DC6B4F8E60B8CF4C] https://deb.ln-ask.me/beta buster common local desktop" | tee -a /etc/apt/sources.list.d/cryptoanarchy.list > /dev/null
+fi
+
 apt update
