e9f5d91f59f424627e1bff886321dd9368c79f86
jan dvorak <jan.dvorak@btcplatby.cz>
2023-11-21 07:52:07 +0100

    update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -1,1 +1,11 @@
-sudo apt install -y btcpayserver btc-rpc-explorer electrs  btc-transmuter ridetheln thunderhub selfhost-dashboard
+#!/bin/bash
+
+SCRIPT=`basename "$0"`
+
+if [[ $UID != 0 ]]; then
+    echo "Must be run as root. Please re-run this script as"
+    echo "  sudo ./$SCRIPT"
+    exit 1
+fi
+
+apt install -y btcpayserver btc-rpc-explorer electrs  btc-transmuter ridetheln thunderhub selfhost-dashboard
