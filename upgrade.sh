77db11f34ca78b7641c28bd71b4b7fa65664ee45
jan dvorak <jan.dvorak@btcplatby.cz>
2023-11-21 08:05:35 +0100

    add upgrade.sh

---
+++ upgrade.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT=`basename "$0"`
+
+if [[ $UID != 0 ]]; then
+    echo "Must be run as root. Please re-run this script as"
+    echo "  sudo ./$SCRIPT"
+    exit 1
+fi
+
+apt update
+apt dist-upgrade
