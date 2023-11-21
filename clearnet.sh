7d0af274773ce37dc778513401111dece9d983bf
jan dvorak <jan.dvorak@btcplatby.cz>
2023-11-21 08:17:57 +0100

    add clearnet.sh

---
+++ clearnet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT=`basename "$0"`
+
+if [[ $UID != 0 ]]; then
+    echo "Must be run as root. Please re-run this script as"
+    echo "  sudo ./$SCRIPT"
+    exit 1
+fi
+
+apt install -y selfhost-clearnet
