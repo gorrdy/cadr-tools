eb0f6eaab9da22c917e1251e22d33640a6a717ea
Jan Dvorak <dvorakh1997@gmail.com>
2023-12-26 00:00:48 +0100

    add upgrade_debian.sh to upgrade from deb10 to deb11

---
+++ upgrade_debian.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+SCRIPT=`basename "$0"`
+
+if [[ $UID != 0 ]]; then
+    echo "Must be run as root. Please re-run this script as"
+    echo "sudo ./$SCRIPT"
+    exit 1
+fi
+
+apt update
+apt dist-upgrade -y
+apt autoremove --purge
+apt clean
+
+systemctl stop lnd-system-mainnet
+
+sed -i 's/buster\/updates/bullseye-security/g' /etc/apt/sources.list
+sed -i 's/10\/prod buster/11\/prod bullseye/g' /etc/apt/sources.list
+sed -i 's/buster/bullseye/g' /etc/apt/sources.list
+
+sed -i 's/buster\/updates/bullseye-security/g' /etc/apt/sources.list.d/*
+sed -i 's/10\/prod buster/11\/prod bullseye/g' /etc/apt/sources.list.d/*
+sed -i 's/buster/bullseye/g' /etc/apt/sources.list.d/*
+
+apt update
+apt dist-upgrade -y
+
+systemctl start lnd-system-mainnet
+
+pg_dropcluster --stop 13 main
+pg_upgradecluster 11 main
+pg_dropcluster --stop 11 main
+
+apt autoremove --purge -y
+apt clean
+
+reboot
